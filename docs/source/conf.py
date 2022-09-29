f4dc9adb0e75e2abc4a9a1d6afe4edb5b962d3dd
Kyle Shores <kyle.shores44@gmail.com>
2022-09-29 12:30:46 -0500

    changing version number (#193)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, NCAR/UCAR'
 author = 'NCAR/UCAR'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1'
+release = 'v0.2'
 
 
 # -- General configuration ---------------------------------------------------
